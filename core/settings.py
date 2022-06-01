648ae6a41ae34ff63a0b64067791d5c91a30c86e
Abdul Rehman <85864680+ardevpk@users.noreply.github.com>
2022-06-01 01:45:17 -0700

    updated password

--- core/settings.py
+++ core/settings.py
@@ -179,4 +179,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "adnan1470369258@gmail.com"
-EMAIL_HOST_PASSWORD = "bhjnxdwguzrwzkqw"
+EMAIL_HOST_PASSWORD = "wuwytstulqkyjkhv"
